a8fb22ee9ec858a436753436e17de20a210d97fa
Guido Rann <guidorann.code@gmail.com>
2024-06-13 19:17:32 -0300

    Initial commit

---
+++ src/main/java/com/nrdChat/NrdChatApplication.java
@@ -0,0 +1,13 @@
+package com.nrdChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrdChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NrdChatApplication.class, args);
+	}
+
+}
