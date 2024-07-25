561bc6d7be76f8d643f669dc1bcf5c20d361e03e
Guido Rann <guidorann.code@gmail.com>
2024-07-25 12:01:10 -0300

    adding userController

--- src/main/java/com/nrdChat/app/controller/UserController.java
+++ src/main/java/com/nrdChat/app/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.nrdChat.app.controller;
 
-import com.nrdChat.app.dtos.UserDTO;
-import com.nrdChat.app.service.UserChatService;
+import com.nrdChat.app.dtos.ReqRes;
+import com.nrdChat.app.model.UserChat;
+import com.nrdChat.app.service.UserManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(value = "http://localhost:5173")
@@ -11,11 +15,44 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    UserChatService userChatService;
+    UserManagementService userManagementService;
 
-    @PostMapping("/createUser")
-    public UserDTO createUser(@RequestBody UserDTO user) {
-        return userChatService.createUser(user);
+    @PostMapping("/auth/register")
+    public ResponseEntity<ReqRes> registerUser(@RequestBody ReqRes registrationRequest) {
+        ReqRes resp = userManagementService.registerUser(registrationRequest);
+        return ResponseEntity.ok(resp);
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<ReqRes> loginUser(@RequestBody ReqRes loginRequest) {
+        ReqRes resp = userManagementService.loginUser(loginRequest);
+        return ResponseEntity.ok(resp);
+    }
+
+    @GetMapping("/user/get-user/{userId}")
+    public ResponseEntity<ReqRes> getUsers(@PathVariable Long userId) {
+        ReqRes resp = userManagementService.getUserById(userId);
+        return ResponseEntity.ok(resp);
+    }
+
+    @PutMapping("/adminuser/update-user/{userId}")
+    public ResponseEntity<ReqRes> updateUser(@PathVariable Long userId, @RequestBody UserChat updatedUserChat) {
+        ReqRes resp = userManagementService.updateUser(userId, updatedUserChat);
+        return ResponseEntity.ok(resp);
+    }
+
+    @GetMapping("/user/get-profile")
+    public ResponseEntity<ReqRes> getMyProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        ReqRes resp = userManagementService.getMyInfo(email);
+        return ResponseEntity.status(resp.getStatusCode()).body(resp);
+    }
+
+    @DeleteMapping("/admin/delete/{userId}")
+    public ResponseEntity<ReqRes> deleteUser(@PathVariable Long userId) {
+        ReqRes resp = userManagementService.deleteUserById(userId);
+        return ResponseEntity.ok(resp);
     }
 
 }
--- src/main/java/com/nrdChat/app/service/IUserChatService.java
+++
@@ -1,8 +0,0 @@
-package com.nrdChat.app.service;
-
-import com.nrdChat.app.dtos.UserDTO;
-
-public interface IUserChatService {
-
-    public UserDTO createUser(UserDTO userChat);
-}
